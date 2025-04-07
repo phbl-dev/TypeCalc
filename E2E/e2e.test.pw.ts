9c90c11730ec108d3fed3578adcc5feac6e3c798
Philip Boesen Blomholt <phbl@itu.dk>
2025-04-07 10:20:02 +0200

    Fixed a few things

--- E2E/e2e.test.pw.ts
+++ E2E/e2e.test.pw.ts
@@ -2,14 +2,13 @@ import { test, expect } from '@playwright/test';
 
 
 
-test("Insert 10 + 10", async ({ page }) => {
+
+test("Insert C2 = 10 * 10, A2 = C2 + 1", async ({ page }) => {
   // Navigate to your page
   await page.goto('http://localhost:5173/');
 
-  // Target the specific cell using its ID
   const cell = page.locator('div#C2.Cell');
 
-  // Verify the cell exists and is visible
   await expect(cell).toBeVisible();
 
   await cell.click();
@@ -39,3 +38,62 @@ test("Insert 10 + 10", async ({ page }) => {
 
 });
 
+test("Sum of A1... A5", async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+
+  const A1 = page.locator('div#A1.Cell');
+
+  await A1.click();
+
+  await A1.type("1")
+
+  const A2 = page.locator('div#A2.Cell');
+
+  await A2.click();
+
+  await A2.type("1")
+
+
+  const A3 = page.locator('div#A3.Cell');
+
+  await A3.click();
+
+  await A3.type("1")
+
+  const A4 = page.locator('div#A4.Cell');
+  await A4.click();
+  await A4.type("1")
+  const A5 = page.locator('div#A5.Cell');
+  await A5.click();
+  await A5.type("1")
+
+  const A6 = page.locator('div#A6.Cell');
+
+  await A6.click()
+
+  await A6.type("=SUM(A1:A5)")
+
+  await page.keyboard.press('Enter');
+
+  await expect(A6).toContainText('5');
+
+})
+
+
+
+test("CHOOSE", async ({ page }) => {
+  // Navigate to your page
+  await page.goto('http://localhost:5173/');
+
+  const A1 = page.locator('div#A1.Cell');
+
+  await A1.click();
+
+  await A1.type("=SUM(CHOOSE(1,10,20,30))")
+
+  await page.keyboard.press('Enter');
+
+  await expect(A1).toContainText('10');
+
+
+})
