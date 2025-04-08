f7a8d726b9c33ae8582960a1b5fc18aafde1190b
Philip Boesen Blomholt <phbl@itu.dk>
2025-04-08 18:32:22 +0200

    Fixed CellAreaRecalculate. The Error was in SeenBefore.

--- src/back-end/Expressions.ts
+++ src/back-end/Expressions.ts
@@ -735,7 +735,7 @@ class RefSet {
         if (arg1 instanceof CellRef) {
             return !this.cellRefsSeen.add(arg1 as CellRef);
         } else {
-            return !this.cellAreasSeen.has(arg1 as CellArea);
+            return !this.cellAreasSeen.add(arg1 as CellArea);
         }
     }
 }
@@ -862,10 +862,10 @@ export class CellRef extends Expr implements IEquatable<CellRef> {
         let referred: Interval;
         let supported: (arg: number) => Interval;
 
-        if (abs) { // case abs
+        if (abs) {
             referred = new Interval(ra, ra);
             supported = (_r) => new Interval(r1, r2);
-        } else {   // case rel
+        } else {
             referred = new Interval(r1 + ra, r2 + ra);
             supported = (r) => new Interval(r - ra, r - ra);
         }
@@ -874,7 +874,6 @@ export class CellRef extends Expr implements IEquatable<CellRef> {
     }
 }
 
-// Should it inherit from IEquatable<CellArea>?
 export class CellArea extends Expr implements IEquatable<CellArea> {
     private readonly ul: SuperRARef;
     private readonly lr: SuperRARef;
@@ -971,14 +970,14 @@ export class CellArea extends Expr implements IEquatable<CellArea> {
 
         if(referredCols.length() < referredRows.length()) {
             referredCols.forEach((c) => {
-                const suppCols = supportedCols(c);
+                let suppCols = supportedCols(c);
                 referredRows.forEach((r) => {
                     referredSheet.AddSupport(c, r, supported, suppCols, supportedRows(r));
                 });
             });
         } else {
             referredRows.forEach((r) => {
-                const suppRows = supportedRows(r);
+                let suppRows = supportedRows(r);
                 referredCols.forEach((c) => {
                     referredSheet.AddSupport(c, r, supported, supportedCols(c), suppRows);
                 });
@@ -990,7 +989,7 @@ export class CellArea extends Expr implements IEquatable<CellArea> {
         if (ulAbs) {
             if (lrAbs) {
                 [ra, rb] = [Math.min(ra, rb), Math.max(ra, rb)];
-                return [new Interval(ra, rb), (r) => new Interval(ra, rb)];
+                return [new Interval(ra, rb), _ => new Interval(r1, r2)];
             } else {
                 return [new Interval(Math.min(ra, r1 + rb), Math.max(ra, r2 + rb )),
                     (r) => ra < r ? new Interval(Math.max(r1, r - rb),r2):
@@ -998,7 +997,6 @@ export class CellArea extends Expr implements IEquatable<CellArea> {
                             new Interval(r1 ,r2)
                 ]
             }
-
         } else {
             if (lrAbs) {
                 return [new Interval(Math.min(ra, r1 + rb),
@@ -1009,7 +1007,6 @@ export class CellArea extends Expr implements IEquatable<CellArea> {
                 [ra, rb] = [Math.min(ra, rb), Math.max(ra, rb)];
                 return [new Interval(r1 +ra, r2 + rb), (r) => new Interval(Math.max(r1, r - rb), Math.min(r2, r - ra))]
             }
-
         }
     }
 
