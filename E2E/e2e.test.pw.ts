4d0150046b09890c3334be9b40f62218a3cc2333
Jacob Breum <jacob.breum@hotmail.com>
2025-04-11 14:28:35 +0200

    Removed keyboard.down and up

--- E2E/e2e.test.pw.ts
+++ E2E/e2e.test.pw.ts
@@ -192,29 +192,21 @@ test("DnD", async ({ page }) => {
 
   const A1 = page.locator("div#A1.Cell")
   await A1.click()
-  await page.keyboard.down("Control")
-  await page.keyboard.press("C")
-  await page.keyboard.up("Control")
+  await page.keyboard.press("Control+C")
 
   const B1 = page.locator("div#B1.Cell")
   await B1.click()
-  await page.keyboard.down("Control")
-  await page.keyboard.press("X")
-  await page.keyboard.up("Control")
+  await page.keyboard.press("Control+X")
   await expect(B1).toContainText('10');
 
   const A2 = page.locator("div#A2.Cell")
   await A2.click()
-  await page.keyboard.down("Control")
-  await page.keyboard.press("C")
-  await page.keyboard.up("Control")
+  await page.keyboard.press("Control+C")
 
   const C2 = page.locator("div#C2.Cell")
   await C2.click()
 
-  await page.keyboard.down("Control")
-  await page.keyboard.press("V")
-  await page.keyboard.up("Control")
+  await page.keyboard.press("Control+V")
 
   await expect(A2).toContainText('20');
   await expect(C2).toContainText('20');
