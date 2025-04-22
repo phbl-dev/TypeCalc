bef9ca54bc26a45089a343476aa2fe5b82c98984
Philip Boesen Blomholt <phbl@itu.dk>
2025-04-22 18:45:08 +0200

    Fucking shit. it works v2

--- src/front-end/SpreadsheetGrid.tsx
+++ src/front-end/SpreadsheetGrid.tsx
@@ -1,14 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { VariableSizeGrid as Grid } from "react-window";
 import {
-    GetRawCellContent,
-    GetSupportsInWindow,
-    ParseToActiveCell,
-    ShowWindowInGUI,
-    WorkbookManager,
-    XMLReader
-} from "../WorkbookIO";
-import {
     getCell,
     adjustFormula,
     numberToLetters,
